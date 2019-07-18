e1f1b1b6e532844c5aa4fab4a57f5a06516feadc
Anderson <hkrnew@gmail.com>
2019-07-17 21:28:43 -0300

    🎉 Commit inicial

---
+++ conf/provision/bootstrap.d/30-permissions.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+# chown 1000:1000 -R /var/www/html
---
+++ conf/provision/entrypoint.d/20-vsftp.sh
@@ -0,0 +1,16 @@
+# #!/usr/bin/env bash
+
+# if ! id -u "$FTP_USER" > /dev/null 2>&1; then
+#     # Add group
+#     groupadd -g "$FTP_GID" "$FTP_USER"
+
+#     # Add user
+#     useradd -u "$FTP_UID" --shell /bin/bash --no-user-group "$FTP_USER"
+
+#     # Assign user to group
+#     usermod -g "$FTP_USER" "$FTP_USER"
+
+#     # Set passwords to "dev"
+#     echo "$FTP_USER":"$FTP_PASSWORD" | chpasswd
+# fi
+
